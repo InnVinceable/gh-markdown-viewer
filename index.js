9e0119a21c34c4241df9c5dacf9363b41b248e3a
Vincent <vincentdegoisdev@gmail.com>
2019-07-22 18:54:44 +0200

    Wired up component

---
+++ index.js
@@ -0,0 +1,11 @@
+import MarkdownViewer from './src/Components/Viewer';
+
+import GithubMarkdownFileTree from './src/Components/TreeList';
+
+import GithubMarkdownViewer from './src/Components/TreeViewer';
+
+export {
+    MarkdownViewer,
+    GithubMarkdownFileTree,
+    GithubMarkdownViewer
+}
